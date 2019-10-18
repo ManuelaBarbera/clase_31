53fa7c96e2dac07df8dc85f6836ed130fb3a29d7
Manuela Barbera <manuelabarbera@MacBook-Air-de-Manuela.local>
2019-10-18 20:19:39 -0300

    ejercicios

---
+++ operadores.js
@@ -0,0 +1,48 @@
+//Ejercicio 27//
+
+let numero1 = 6;
+let numero2 = 4;
+
+console.log (numero1 + numero2);
+console.log (numero1 - numero2);
+console.log (numero1 * numero2);
+console.log (numero1 / numero2);
+console.log (numero1 % numero2);
+
+
+//Ejercicio 28//
+
+let numero3 = 9;
+
+console.log (numero3 *1);
+console.log (numero3 *2);
+console.log (numero3 *3);
+console.log (numero3 *4);
+console.log (numero3 *5);
+console.log (numero3 *6);
+console.log (numero3 *7);
+console.log (numero3 *8);
+console.log (numero3 *9);
+
+
+//Ejercicio 29//
+let longitud = 10;
+console.log (longitud * 4);
+
+//ejercicio 30//
+let lado = 5;
+console.log (lado ** 2);
+
+//Ejercicio 31//
+let lado1 = 10;
+let lado2 = 20;
+let lado3 = 5;
+
+console.log (lado1 + lado2 + lado3);
+
+//Ejercicio 32//
+let altura = 10;
+let base = 4;
+
+console.log (altura + base)
+console.log (altura * base)
