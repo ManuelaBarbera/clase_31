f1ed8889d27d78b61b97fce0990ec474d70ffd75
Manuela Barbera <manuelabarbera@MacBook-Air-de-Manuela.local>
2019-10-18 22:01:04 -0300

    ejercicio35

--- operadores.js
+++ operadores.js
@@ -1,5 +1,4 @@
 //Ejercicio 27//
-
 let numero1 = 6;
 let numero2 = 4;
 
@@ -11,7 +10,6 @@ console.log (numero1 % numero2);
 
 
 //Ejercicio 28//
-
 let numero3 = 9;
 
 console.log (numero3 *1);
@@ -27,10 +25,12 @@ console.log (numero3 *9);
 
 //Ejercicio 29//
 let longitud = 10;
+
 console.log (longitud * 4);
 
 //ejercicio 30//
 let lado = 5;
+
 console.log (lado ** 2);
 
 //Ejercicio 31//
@@ -48,12 +48,18 @@ console.log (altura + base);
 console.log (altura * base);
 
 //Ejercicio 33//
-
 let cantidadDePersonas = 100;
 
-cantidadDePersonas+= 5
+cantidadDePersonas+= 5;
 console.log("la cantidad de personas es: " + cantidadDePersonas)
-cantidadDePersonas-= 3
+cantidadDePersonas-= 3;
 console.log("la cantidad de personas es: " + cantidadDePersonas)
-cantidadDePersonas*= 2
+cantidadDePersonas*= 2;
 console.log("la cantidad de personas es: " + cantidadDePersonas)
+
+//Ejercicio 35
+let numeroDeCliente = 143245;
+let numeroDeClienteIngresado = 143245;
+
+console.log ("El número ingresado por el cliente es correcto:", numeroDeCliente == 143245)
+console.log ("El cliente ingresó un número de cliente válido:", numeroDeClienteIngresado === 143245)
